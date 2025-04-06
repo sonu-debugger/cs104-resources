62374af280d759ac2c2f5c49ceee600f6134dd4c
Banothu Sonusree <sonusree.b2006@gmail.com>
2025-04-06 14:50:07 +0530

    Add files via upload

---
+++ 9-2/357/labDirectory/complex.py
@@ -0,0 +1,19 @@
+'''
+    Complex Numbers
+    Author: Saksham Rathi
+'''
+
+class Complex:
+
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
+
+    def __str__(self):
+        return f"{self.x} + i{self.y}"
+
+    def __add__(self, a):
+        return Complex(self.x + a.x, self.y + a.y)
+
+    def __sub__(self, a):
+        return Complex(self.x - a.x, self.y - a.y)
