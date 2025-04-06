5c98368f4a194e0afb8e4fc72d5cf182f71042ec
Banothu Sonusree <sonusree.b2006@gmail.com>
2025-04-06 15:01:22 +0530

    Add files via upload

---
+++ 10-2/377/labDirectory/main.py
@@ -0,0 +1,21 @@
+'''
+    Distributions
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Seeding for reproducibility
+
+
+# sampling from each of the six distributions
+
+
+# plotting histograms for each of the distributions
+# plt.subplot(3,2,1)
+
+
+# adjust the sub-plots to fit the titles and labels
+plt.tight_layout()
+# save the plot as plot.png
+plt.savefig('plot.png')
